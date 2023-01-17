32e75306237aacc96601646cf603e9dbf406829f
Connor McCue <connor.mccue@comcast.net>
2023-01-17 15:23:03 -0800

    shopping list item jsx

--- src/components/ShoppingList/ShoppingListItem.jsx
+++ src/components/ShoppingList/ShoppingListItem.jsx
@@ -1,3 +1,12 @@
-export default function ShoppingListItem() {
-  return <></>;
+export default function ShoppingListItem({ item }) {
+  return (
+    <>
+      <div>
+        {item.item_name}
+        {item.quantity}
+        {item.shopping_list_id}
+      </div>
+    </>
+
+  );
 }
